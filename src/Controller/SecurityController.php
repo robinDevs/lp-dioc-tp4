78b970fd65eba6d25e293604e4288ee1c50e5707
Deruyck Robin <robin_deruyck@ens.univ-artois.fr>
2017-11-15 11:07:02 +0100

    Draft

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -37,5 +37,29 @@ class SecurityController extends Controller
     {
         // FIXME: Instancier le formulaire et à la soumission enregistrer le user.
         // La vue à rendre : Security/register.html.twig
+        // create a task and give it some dummy data for this example
+        $user = new User();
+                    $form=  $this->createFormBuilder($user)
+                    ->add('firstname')
+                    ->add('lastname')
+                    ->add("email")
+                    ->add('birthday')
+                    ->add('password')
+                    ->add('createdat')
+                    ->add('sumbitUser',array('label'=>'S\'enregistrer'))
+                    ->getForm();
+        $form = $this->createForm(PersonType::class, $user);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $personne->setCreatedAt(new \Datetime('now'));
+            $em->persist($personne);
+            $em->flush();
+        }
+
+        return $this->render("Personne/new.html.twig",array('form'=>$form->createView()));
+    }
+
+        return $this->render('Security/register.html.twig',);
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,7 +9,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class UserController extends Controller
 {
     /**
-     * // FIXME: la route doit être /my_profile.
+     * @Route(
+     *     path="/my_profile",
+     *     name="my.profile"
+     * )
      */
     public function myProfileAction()
     {
@@ -17,12 +20,20 @@ class UserController extends Controller
     }
 
     /**
-     * // FIXME: la route doit être /profile/3 par exemple.
+     * // FIXME: la route doit être /profile/3 par exemple
+     * @Route(
+     *     path="/profile/{id}",
+     *     name="profile_page"
+     * )
      */
     public function profileAction(User $user)
     {
         // FIXME: un utilisateur connecté qui se rend sur sa propre page est redirigé vers /my_profile
 
+
+        if($this->getUser()==$user) {
+            return $this->redirectToRoute("my.profile");
+        }
         return $this->render('User/profile.html.twig', ['user' => $user]);
     }
 }
